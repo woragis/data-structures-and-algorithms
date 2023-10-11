f4133d447428f7671c3dfb435c68dd827644594d
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-11 04:08:50 -0300

    Create Exponential Search
    Time complexity: O(log n) Space complexity: O(log n) with recursive Binary Search and O(1) with iterative Binary search

---
+++ algorithms/searching/7-exponential_search.py
@@ -0,0 +1,20 @@
+def _binarySearch(arr, key, left, right):
+    if left <= right:
+        mid = left + (right-left) // 2
+
+        if arr[mid] == key:
+            return mid
+        if arr[mid] > key:
+            return _binarySearch(arr, key, left, mid-1)
+        return _binarySearch(arr, key, mid+1, right)
+    return -1
+
+
+def exponentialSearch(arr, key):
+    if arr[0] == key:
+        return 0
+    i = 1
+    while i < n and arr[i] <= key:
+        i *= 2
+
+    return _binarySearch(arr, key, i//2, min(i, len(arr)-1))
