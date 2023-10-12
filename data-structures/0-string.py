462463063854fa5f2a332f3564ca373f38b60f53
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-11 23:50:15 -0300

    String Data structure

---
+++ data-structures/0-string.py
@@ -0,0 +1,58 @@
+def firstIndex(string: str, c: str) -> int:
+    if string.index(c):
+        index = string.index(c)
+        return index
+    return -1
+
+
+def lastIndex(string: str, c: str) -> int:
+    string, c = string[::-1], c[::-1]
+    if string.index(c):
+        index = string.index(c)
+        return abs(index-len(string)+1)
+    return -1
+
+
+def reverseStringRecursive(string: str) -> str:
+    ans = ""
+    string = list(string)
+    while string:
+        current_last = string.pop()
+        ans += current_last
+    print(ans)
+    return ans
+
+
+def _leftRotate(string: str) -> str:
+    lst = list(string)
+    first_element = lst.pop(0)
+    lst.append(first_element)
+    string = ''.join(lst)
+    return string
+
+
+def leftRotate(string: str, times: int) -> str:
+    while times:
+        times -= 1
+        string = _leftRotate(string)
+    return string
+
+
+def _rightRotate(string: str) -> str:
+    lst = list(string)
+    last_element = lst.pop()
+    lst.insert(0, last_element)
+    string = ''.join(lst)
+    return string
+
+
+def rightRotate(string: str, times: int) -> str:
+    while times:
+        times -= 1
+        string = _rightRotate(string)
+    return string
+
+
+if __name__ == '__main__':
+    myString = 'Jezreel de Andrade Galvao Veloso'
+    print(leftRotate(myString, 10))
