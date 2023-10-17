774286cc2eec535e0c0f9b0d825fd4e3b572162b
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-17 19:09:48 -0300

    Init graph intstructions

---
+++ data-structures/graph/graph.py
@@ -0,0 +1,45 @@
+# networks
+# types of graphs:
+#   undirected graph, no edge orientation
+#   directed graph / digraph, edges have direction
+#   weighted graph, come in directed or undirected
+# special graphs:
+#   trees
+#   directed acycled graphs DAGS
+#   bipartite graph
+#   complete graph
+
+# shortest path algorithms:
+#   BFS (unweighted graph)
+#   Dijkstra's Algorithm
+#   Bellman-Ford
+#   Floyd-Warshall
+#   A*
+#   and many more
+
+# Connectivity:
+#   any search algorithm
+#   DFS or BFS
+
+# Negative cycles:
+#   Bellman-Ford
+#   Floyd-Warshall
+
+# Strong Connected Components
+#   Tarjan's Algorithm
+#   Kosaraju's Algorithm
+
+# Traveling salesman problem
+#   Held-Karp
+#   Branch and bound
+#   and many aproximation algorithms
+
+# Articulation points
+# Minimum spanning tree (MST)
+#   Kruskal's Algorithm
+#   Prim's & Boruvka's Algorithm
+
+# Network flow / maximum flow
+#   Ford-Fulkerson
+#   Edmonds-Karp
+#   Dicnic's Algorithm
