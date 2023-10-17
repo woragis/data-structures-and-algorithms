895c2e8b090874c01266715186e8e53732245375
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-17 16:12:56 -0300

    improve merge sort

--- training/algorithm/mergeSort.py
+++ training/algorithm/mergeSort.py
@@ -68,6 +68,63 @@ def merge2(arr):
             k += 1
 
 
+def merge3(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+        merge3(left_arr)
+        merge3(right_arr)
+        i, j, k = 0, 0, 0
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] <= right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+
+def merge4(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+        merge4(left_arr)
+        merge4(right_arr)
+        i, j, k = 0, 0, 0
+        # iterate through all the subarrays
+        while i < len(left_arr) and j < len(right_arr):
+            # implement left[i] if it's smaller
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            # implement right[j] if it's smaller
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+        # check if there was any element left in the smaller sub array
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        # check if there was any element left in the bigger sub array
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+
 def randomArray(arr: list, limit: int):
     from random import randint
     for n in range(limit):
